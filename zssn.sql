b178ce23ad3983e894c39ce073d61f90740e37ae
francisco <francisco.ortega@webppi.com>
2022-07-18 12:01:47 -0500

    Update: item's added to table item's

--- zssn.sql
+++ zssn.sql
@@ -10,53 +10,7 @@ DELETE cascade, constraint fk_item_id foreign key (item_id) references item(id)
 ON
 DELETE cascade );
 
-SELECT  *
-FROM inventory inv
-INNER JOIN item it
-ON it.id = inv.item_id
-WHERE inv.survivor_id IN (1, 3);
-
-SELECT  COUNT(*) AS survivors
-FROM survivor
-WHERE infected = false
-UNION
-SELECT  COUNT(*) AS nonsurvivors
-FROM survivor
-WHERE infected = true;
-
-SELECT  *
-FROM inventory
-INNER JOIN survivor
-ON inventory.survivor_id = survivor.id
-WHERE inventory.item_id = 1
-AND survivor.infected = 0
-UNION
-SELECT  *
-FROM inventory
-INNER JOIN survivor
-ON inventory.survivor_id = survivor.id
-WHERE inventory.item_id = 2
-AND survivor.infected = 0
-UNION
-SELECT  *
-FROM inventory
-INNER JOIN survivor
-ON inventory.survivor_id = survivor.id
-WHERE inventory.item_id = 3
-AND survivor.infected = 0
-UNION
-SELECT  *
-FROM inventory
-INNER JOIN survivor
-ON inventory.survivor_id = survivor.id
-WHERE inventory.item_id = 4
-AND survivor.infected = 0;
-
-SELECT  SUM(iv.quantity) AS quantity
-       ,SUM(it.points)   AS points
-FROM inventory iv
-INNER JOIN item it
-ON it.id = iv.item_id
-INNER JOIN survivor sur
-ON sur.id = iv.survivor_id
-WHERE sur.infected = 1;
+insert into item values ("Water",4),
+("Food",3),
+("Medication",2),
+("Ammunition",1);
