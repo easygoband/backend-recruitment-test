fc3f22ca602e6bd63b60e170e1a408e4af2b4f0a
hector <dev.hectorlemus@gmail.com>
2022-08-10 16:49:37 -0500

    - survivor jpa repository

---
+++ src/main/java/com/dev/zssn/repository/SurvivorRepository.java
@@ -0,0 +1,9 @@
+package com.dev.zssn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.zssn.models.Survivor;
+
+public interface SurvivorRepository extends JpaRepository<Survivor, Long> {
+
+}
