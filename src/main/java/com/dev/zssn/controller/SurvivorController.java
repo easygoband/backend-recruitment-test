95a35fb50f2a3f911c42a700fabf81fcdcfee1a7
hector <dev.hectorlemus@gmail.com>
2022-08-10 23:33:50 -0500

    - POST method to register survivor - Survivors from service

--- src/main/java/com/dev/zssn/controller/SurvivorController.java
+++ src/main/java/com/dev/zssn/controller/SurvivorController.java
@@ -2,24 +2,49 @@ package com.dev.zssn.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.zssn.models.Survivor;
-import com.dev.zssn.repository.SurvivorRepository;
+import com.dev.zssn.dto.SurvivorDto;
+import com.dev.zssn.services.SurvivorService;
 
 @RestController
 class SurvivorController {
 
-  private final SurvivorRepository repository;
+  final Logger LOGGER = LoggerFactory.getLogger(SurvivorController.class);
 
-  SurvivorController(SurvivorRepository repository) {
-    this.repository = repository;
+  private final SurvivorService service;
+
+  SurvivorController(SurvivorService repository) {
+    this.service = repository;
   }
 
   @GetMapping("/survivors")
-  public List<Survivor> all() {
-    return repository.findAll();
+  public ResponseEntity<List<SurvivorDto>> all() {
+    try {
+      final List<SurvivorDto> survivors = service.all();
+      return new ResponseEntity<>(survivors, HttpStatus.OK);
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage());
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PostMapping("/survivors")
+  public ResponseEntity<SurvivorDto> registerSurvivor(@RequestBody SurvivorDto survivor) {
+    try {
+      final SurvivorDto savedSurvivor = service.registerSurvivor(survivor);
+      return new ResponseEntity<>(savedSurvivor, HttpStatus.OK);
+    } catch (Exception e) {
+      LOGGER.error(e.getMessage());
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
 }
