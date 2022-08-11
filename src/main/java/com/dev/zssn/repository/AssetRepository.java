9500ea4b14b5559895057672061c055cfd48f8af
hector <dev.hectorlemus@gmail.com>
2022-08-10 23:31:48 -0500

    - find asset by name

--- src/main/java/com/dev/zssn/repository/AssetRepository.java
+++ src/main/java/com/dev/zssn/repository/AssetRepository.java
@@ -1,9 +1,13 @@
 package com.dev.zssn.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dev.zssn.models.Asset;
 
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+
+  List<Asset> findByName(String name);
 
 }
