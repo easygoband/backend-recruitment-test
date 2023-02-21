245488ac8775ae3d653e7e5dae9f661418930079
David Torres <davidfernandotorres07@gmail.com>
2023-02-21 17:52:08 -0500

    feat: Average amount of each kind of resource by survivor

--- src/main/java/com/easygo/david/easygotest/controllers/StatisticsController.java
+++ src/main/java/com/easygo/david/easygotest/controllers/StatisticsController.java
@@ -1,5 +1,6 @@
 package com.easygo.david.easygotest.controllers;
 
+import com.easygo.david.easygotest.models.SurvivorInventory;
 import com.easygo.david.easygotest.services.StatisticsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/survivors/statistics")
@@ -26,4 +31,19 @@ public class StatisticsController {
     ResponseEntity<Double> percentageOfInfectedSurvivors(@PathVariable("infected") Boolean infected) {
         return new ResponseEntity<>(statisticsService.getInfectedSurvivorsRate(infected), HttpStatus.OK);
     }
+
+
+
+    @GetMapping("/avg-res-amount")
+    @Operation(summary = "Average amount of resources.", description = "Average amount of each kind of resource of all survivors.")
+    ResponseEntity<Map<String, String>> averageAmountForResources() {
+        return new ResponseEntity<>(statisticsService.avgAmountOfResources(), HttpStatus.OK);
+    }
+
+    @GetMapping("/avg-res-amount/{item_name}")
+    @Operation(summary = "Average amount for specific resource.", description = "Average amount for specific resource of all survivors.")
+    ResponseEntity<String> averageAmountForSpecificResource(@PathVariable("item_name") String item) {
+        return new ResponseEntity<>(statisticsService.avgAmountOfResource(item.toLowerCase()), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/easygo/david/easygotest/services/StatisticsService.java
+++ src/main/java/com/easygo/david/easygotest/services/StatisticsService.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 @AllArgsConstructor
 public class StatisticsService {
@@ -13,11 +15,42 @@ public class StatisticsService {
 
 
     public Double getInfectedSurvivorsRate(boolean infected) {
-        double total =  inventoriesService.getAllInventories().size();
-        double target =  inventoriesService.getAllInventoriesCustom(infected).size();
+        double total = inventoriesService.getAllInventories().size();
+        double target = inventoriesService.getAllInventoriesCustom(infected).size();
 
         System.out.println(total);
         System.out.println(target);
-        return (target/total)*100;
+        return (target / total) * 100;
     }
+
+    public Map<String, String> avgAmountOfResources() {
+        var target = inventoriesService.getAllInventoriesCustom(false);
+
+        Map<String, Integer> allItems = new HashMap<>();
+        target.forEach(survivorInventory -> {
+            survivorInventory.getInventory_item().forEach(invItem -> {
+                int current = allItems.getOrDefault(invItem.getItem().getName(),0);
+                allItems.put(invItem.getItem().getName(), current + invItem.getQuantity());
+            });
+        });
+
+        Map<String,String> itemsPerSurvivor = new HashMap<>();
+        int totalNonInfected = inventoriesService.getAllInventoriesCustom(false).size();
+        System.out.println("Total survivors="+totalNonInfected);
+
+        allItems.forEach((k, v) -> {
+            System.out.println(k + "->" +  v);
+            itemsPerSurvivor.put(k,(v/totalNonInfected)+" per survivor");
+        });
+
+        return itemsPerSurvivor;
+    }
+
+    public String avgAmountOfResource(String item) {
+        return avgAmountOfResources().get(item);
+    }
+
+
+//    3.  Average amount of each kind of resource by survivor (e.g. 5 waters per survivor)
+//    4.  Points lost because of infected survivor.
 }
