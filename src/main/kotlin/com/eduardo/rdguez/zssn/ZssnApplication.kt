4f9a64387bf052c6a1299958a759092ced60e83a
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-21 13:18:58 -0600

    Initialize a new Spring Boot project

---
+++ src/main/kotlin/com/eduardo/rdguez/zssn/ZssnApplication.kt
@@ -0,0 +1,11 @@
+package com.eduardo.rdguez.zssn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZssnApplication
+
+fun main(args: Array<String>) {
+  runApplication<ZssnApplication>(*args)
+}
---
+++ src/test/kotlin/com/eduardo/rdguez/zssn/ZssnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eduardo.rdguez.zssn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZssnApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
