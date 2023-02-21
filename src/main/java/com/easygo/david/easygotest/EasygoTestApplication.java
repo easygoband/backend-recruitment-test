60f202de1e1f14a965f5ac667e4a940fbab5b83d
David Torres <davidfernandotorres07@gmail.com>
2023-02-21 06:56:59 -0500

    feat: project initialization

---
+++ src/main/java/com/easygo/david/easygotest/EasygoTestApplication.java
@@ -0,0 +1,13 @@
+package com.easygo.david.easygotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasygoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasygoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easygo/david/easygotest/EasygoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easygo.david.easygotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasygoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
