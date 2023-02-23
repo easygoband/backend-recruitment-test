8b171383346dc20c53e6fa6e34ae37481c3cc700
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-23 17:44:54 -0600

    Add api constants class

---
+++ src/main/kotlin/com/eduardo/rdguez/zssn/constant/ApiConstants.kt
@@ -0,0 +1,16 @@
+package com.eduardo.rdguez.zssn.constant
+
+object ApiConstants {
+  /* Regex */
+  const val LETTERS_REGEX: String = "^[A-Za-zÀ-ÿ ]+$"
+
+  /* Infection */
+  const val REPORTING_LIMIT: Int = 3
+
+  /* Numbers */
+  const val ZERO: Int = 0
+  const val TWO: Int = 2
+  const val FIFTY: Int = 50
+  const val ONE_HUNDRED_TWENTY_FIVE: Long = 125
+  const val ONE_HUNDRED: Int = 100
+}
