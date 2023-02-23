2e373ac962587db2d6043bc698a86d5371bf9c92
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-23 17:46:55 -0600

    Add location request

---
+++ src/main/kotlin/com/eduardo/rdguez/zssn/model/request/LocationRequest.kt
@@ -0,0 +1,14 @@
+package com.eduardo.rdguez.zssn.model.request
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import javax.validation.constraints.NotNull
+
+class LocationRequest(
+  @JsonProperty("latitude")
+  @field:NotNull
+  val latitude: Int,
+
+  @JsonProperty("longitude")
+  @field:NotNull
+  val longitude: Int,
+)
