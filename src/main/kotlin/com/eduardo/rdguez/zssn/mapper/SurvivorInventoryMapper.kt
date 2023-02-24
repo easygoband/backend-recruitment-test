74f96725c74b82e949a30eb9daa2858f0a2a6c20
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-23 19:54:39 -0600

    Add survivor-inventory mapper to allow convertion to a entity

--- src/main/kotlin/com/eduardo/rdguez/zssn/mapper/SurvivorInventoryMapper.kt
+++ src/main/kotlin/com/eduardo/rdguez/zssn/mapper/SurvivorInventoryMapper.kt
@@ -1,10 +1,10 @@
 package com.eduardo.rdguez.zssn.mapper
 
-import com.eduardo.rdguez.zssn.domain.SurvivorInventory
 import com.eduardo.rdguez.zssn.domain.Item
 import com.eduardo.rdguez.zssn.domain.Survivor
+import com.eduardo.rdguez.zssn.domain.SurvivorInventory
 
-class InventoryMapper {
+class SurvivorInventoryMapper {
   companion object {
     fun toEntity(survivor: Survivor, item: Item, quantity: Int): SurvivorInventory {
       return SurvivorInventory(
