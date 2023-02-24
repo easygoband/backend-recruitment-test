9c17f0bc292a1f70ecdd111a691f908731e03b04
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-24 13:45:41 -0600

    Include log messages in item service

--- src/main/kotlin/com/eduardo/rdguez/zssn/service/impl/ItemServiceImpl.kt
+++ src/main/kotlin/com/eduardo/rdguez/zssn/service/impl/ItemServiceImpl.kt
@@ -4,14 +4,18 @@ import com.eduardo.rdguez.zssn.domain.Item
 import com.eduardo.rdguez.zssn.repository.ItemRepository
 import com.eduardo.rdguez.zssn.service.ItemService
 import java.util.*
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
 
 @Service
 class ItemServiceImpl(
   private val itemRepository: ItemRepository
 ) : ItemService {
+  private val logger = KotlinLogging.logger {}
 
   override fun findItemByName(name: String): Optional<Item> {
+    logger.debug { "Find item by name: $name" }
+
     return itemRepository.findByNameIgnoreCase(name)
   }
 
