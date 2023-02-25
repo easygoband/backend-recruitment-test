b5ec19c6e02e435f9f18bf655305a62f689cfdca
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-25 10:07:57 -0600

    Add function to map survivor-inventory dto

--- src/main/kotlin/com/eduardo/rdguez/zssn/mapper/SurvivorInventoryMapper.kt
+++ src/main/kotlin/com/eduardo/rdguez/zssn/mapper/SurvivorInventoryMapper.kt
@@ -3,6 +3,7 @@ package com.eduardo.rdguez.zssn.mapper
 import com.eduardo.rdguez.zssn.domain.Item
 import com.eduardo.rdguez.zssn.domain.Survivor
 import com.eduardo.rdguez.zssn.domain.SurvivorInventory
+import com.eduardo.rdguez.zssn.dto.SurvivorInventoryDto
 
 class SurvivorInventoryMapper {
   companion object {
@@ -12,6 +13,15 @@ class SurvivorInventoryMapper {
         item = item,
         quantity = quantity
       )
+    }
+
+    fun toDto(survivorInventory: List<SurvivorInventory>): List<SurvivorInventoryDto> {
+      return survivorInventory.map { inventory ->
+        SurvivorInventoryDto(
+          item = inventory.item.name,
+          quantity = inventory.quantity
+        )
+      }
     }
   }
 }
