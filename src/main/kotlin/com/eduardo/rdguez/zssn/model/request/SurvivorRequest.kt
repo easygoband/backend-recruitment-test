d55c52156ce63cfbc2902d1eb28446cf5f015263
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-25 10:10:49 -0600

    Add field valid to survivor request

--- src/main/kotlin/com/eduardo/rdguez/zssn/model/request/SurvivorRequest.kt
+++ src/main/kotlin/com/eduardo/rdguez/zssn/model/request/SurvivorRequest.kt
@@ -3,6 +3,7 @@ package com.eduardo.rdguez.zssn.model.request
 import com.eduardo.rdguez.zssn.constant.ApiConstants
 import com.eduardo.rdguez.zssn.model.enums.Gender
 import com.fasterxml.jackson.annotation.JsonProperty
+import javax.validation.Valid
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
@@ -25,13 +26,15 @@ data class SurvivorRequest(
 
   @JsonProperty("gender")
   @field:NotNull
-  val gender: Gender = Gender.M,
+  val gender: Gender,
 
   @JsonProperty("last_location")
+  @field:Valid
   @field:NotNull
   val lastLocation: LocationRequest,
 
   @JsonProperty("items")
+  @field:Valid
   @field:NotNull
   val items: ItemsRequest
 )
