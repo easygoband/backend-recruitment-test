ed87ff72f47e15279642a155853cbe131891c8fc
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-02-25 10:08:39 -0600

    Add function to get item value by key name

--- src/main/kotlin/com/eduardo/rdguez/zssn/model/request/ItemsRequest.kt
+++ src/main/kotlin/com/eduardo/rdguez/zssn/model/request/ItemsRequest.kt
@@ -2,6 +2,7 @@ package com.eduardo.rdguez.zssn.model.request
 
 import com.eduardo.rdguez.zssn.constant.ApiConstants
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.*
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 
@@ -9,20 +10,30 @@ data class ItemsRequest(
   @JsonProperty("water")
   @field:NotNull
   @field:Min(ApiConstants.ZERO.toLong())
-  val water: Int,
+  val water: Int?,
 
   @JsonProperty("food")
   @field:NotNull
   @field:Min(ApiConstants.ZERO.toLong())
-  val food: Int,
+  val food: Int?,
 
   @JsonProperty("medication")
   @field:NotNull
   @field:Min(ApiConstants.ZERO.toLong())
-  val medication: Int,
+  val medication: Int?,
 
   @JsonProperty("ammunition")
   @field:NotNull
   @field:Min(ApiConstants.ZERO.toLong())
-  val ammunition: Int
-)
+  val ammunition: Int?
+) {
+  operator fun get(key: String): Int {
+    return when (key.lowercase()) {
+      "water" -> water!!
+      "food" -> food!!
+      "medication" -> medication!!
+      "ammunition" -> ammunition!!
+      else -> 0
+    }
+  }
+}
